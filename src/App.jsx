367e81704219920e0122a9602e3ecc372340c62f
dustin <dustinusey@me.com>
2024-05-29 16:09:25 -0500

    updated bolilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
     <div className="grid place-items-center h-screen bg-zinc-300">
-      <div className="bg-white rounded-lg px-5 py-10 w-full max-w-[500px] text-indigo-500 text-xl text-center">
-        <p>Setup complete - ready to build something new!</p>
+      <div className="bg-white rounded-lg px-5 py-10 w-full max-w-[500px] text-zinc-500 text-xl text-center">
+        <h1 className="text-3xl font-bold">Hello, World!</h1>
       </div>
     </div>
   );
